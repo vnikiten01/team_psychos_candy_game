4a13849159e6deea905fb57f69027e02dff07766
Viktor Nikitenko <vnikiten01@gmail.com>
2014-10-31 17:15:41 -0400

    BAckground image and candy img displayed

---
+++ app/controllers/game_play.rb
@@ -0,0 +1,5 @@
+get '/start_game' do
+  # Look in app/views/index.erb
+  erb :start_game
+end
+
---
+++ public/js/game_play.js
@@ -0,0 +1,28 @@
+function Candy(){
+  // this.height = 10;
+  // this.width = 20;
+  // this.x = 30;
+  // this.y = 30;
+  this.initDisplay();
+}
+
+Candy.prototype.initDisplay = function() {
+  var candyTemplate = "<img class='candy' src='../imgs/test_candy.jpg' >"
+  // <img src="./imgs/test_candy.jpg" alt="Mountain View" style="width:30%;height:auto">
+  $('body').append(candyTemplate)
+}
+
+function Game() {
+  // this.$game_background = $('#game_backzground');
+  this.candy = new Candy();
+}
+
+
+$(document).ready(function() {
+  game = new Game();
+
+})
+
+
+
+
